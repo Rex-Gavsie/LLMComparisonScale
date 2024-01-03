f33989899e4a9376b23330af54f479391a5a0ca4
RexGavsie <91796121+RexGavsie@users.noreply.github.com>
2024-01-03 11:58:27 -0800

    trying to fix issue with importing from human_eval.data
    in evaluate_functional_correctness.py, the from human_eval.data doesn't work

--- HumanEvalIntegration.py
+++ HumanEvalIntegration.py
@@ -7,22 +7,29 @@ UPDATE IT DOES WORK WOOO we can generate samples jsonls, now to score those
 from human_eval.human_eval.data import write_jsonl, read_problems
 from ProofOfConcept import doWhatTomSevenSaidNormalCompletions
 
-problems = read_problems()
 
-def generate_one_completion(prompt):
+
+def generate_one_completion(prompt, exponentialDilution, dilutionProportion, iterations, maxTokens):
     """Idk if this will work as it might do stuff like talking after the code, 
     I hope temp being 0 will prevent that but idk"""
     print(prompt)
-    response = doWhatTomSevenSaidNormalCompletions(prompt, False, 1, 10, 50) 
+    response = doWhatTomSevenSaidNormalCompletions(prompt, exponentialDilution, dilutionProportion, iterations, maxTokens) 
     print(response)
     return response
 
-num_samples_per_task = 1
-samples = [
-    dict(task_id=task_id, completion=generate_one_completion(problems[task_id]["prompt"]))
-    for task_id in problems
-    for _ in range(num_samples_per_task)
-]
-write_jsonl("samples.jsonl", samples)
+
+def runHumanEvalTest35POCDilution(numSamplesPerTask, exponentialDilution, dilutionProportion, iterations, maxTokens) -> None:
+    
+    
+    problems = read_problems()
+    
+    num_samples_per_task = 1
+    samples = [
+        dict(task_id=task_id, completion=generate_one_completion(problems[task_id]["prompt"], exponentialDilution, dilutionProportion, iterations, maxTokens))
+        for task_id in problems
+        for _ in range(num_samples_per_task)
+    ]
+    write_jsonl("samples.jsonl", samples)
 
 
+# runHumanEvalTest35POCDilution(1, False, 1, 10, 50)
