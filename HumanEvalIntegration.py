f1e894a1bde84d199c5d03ae88663eb4c06df982
RexGavsie <91796121+RexGavsie@users.noreply.github.com>
2024-01-03 11:40:52 -0800

    verified and ran human_eval

--- HumanEvalIntegration.py
+++ HumanEvalIntegration.py
@@ -1,17 +1,23 @@
 """
 This is where I'll integrate the dilution code with the human eval tests and maybe with the other stuff
 
-Idk, fuck it, we ball
+XXXX Idk if this works but hopefully it does
+UPDATE IT DOES WORK WOOO we can generate samples jsonls, now to score those
 """
-
-
-#literally just the example code
-from human_eval.data import write_jsonl, read_problems
+from human_eval.human_eval.data import write_jsonl, read_problems
 from ProofOfConcept import doWhatTomSevenSaidNormalCompletions
 
 problems = read_problems()
 
-num_samples_per_task = 200
+def generate_one_completion(prompt):
+    """Idk if this will work as it might do stuff like talking after the code, 
+    I hope temp being 0 will prevent that but idk"""
+    print(prompt)
+    response = doWhatTomSevenSaidNormalCompletions(prompt, False, 1, 10, 50) 
+    print(response)
+    return response
+
+num_samples_per_task = 1
 samples = [
     dict(task_id=task_id, completion=generate_one_completion(problems[task_id]["prompt"]))
     for task_id in problems
@@ -19,7 +25,4 @@ samples = [
 ]
 write_jsonl("samples.jsonl", samples)
 
-def generate_one_completion(prompt):
-    """Idk if this will work as it might do stuff like talking after the code, 
-    I hope temp being 0 will prevent that but idk"""
-    return doWhatTomSevenSaidNormalCompletions(prompt, False, 1, 10, 50) 
+
